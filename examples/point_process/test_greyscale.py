704610d6861ca63d3a0fb718f3e3b34ee9424878
yinzhoux <yinzhoux23@gmail.com>
2026-03-26 16:39:48 +0800

    refactor: grayscale test

--- examples/point_process/test_greyscale.py
+++ examples/point_process/test_greyscale.py
@@ -1,7 +1,9 @@
 from easycv.image import Image
+from easycv.drawer import ImageDrawer
 
-image_path = '../images/stranthen.jpg'
+image_path = '../../images/stranthen.jpg'
 image = Image()
+drawer = ImageDrawer()
 image.from_file(image_path=image_path, image_type='rgb')
 
 print(image.pixels.shape)
@@ -10,4 +12,4 @@ print(image.type)
 print(image.bands_cnt)
 print(image.bands)
 
-image.save_to('../images/greyscale.jpg', convert_to_grayscale=True)
+image.save_to('../../images/greyscale.jpg', convert_to_grayscale=True)
